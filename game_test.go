5695f4167b29caf1b9193d276c4edcf0fc063afb
Sven Uszpelkat <sven.uszpelkat@gmail.com>
2025-09-28 14:50:50 +0200

    Implement test: TestRemovingLines

--- game_test.go
+++ game_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestRemovingLines(t *testing.T) {
 	game := &Game{}
@@ -15,4 +17,27 @@ func TestRemovingLines(t *testing.T) {
 	t.Logf("Lines: %v", lines)
 	game.removeCompleteLines(lines)
 	game.printBoard()
+
+	if !compareBoards(game.Board, [][]int{
+		{0, 0, 0, 0},
+		{0, 1, 0, 0},
+		{0, 1, 0, 0},
+	}) {
+		t.FailNow()
+	}
+}
+
+func compareBoards(board1, board2 [][]int) bool {
+	result := len(board1) > 0 && len(board1) == len(board2) &&
+		len(board1[0]) > 0 && len(board1[0]) == len(board2[0])
+	if result {
+		for ix, b := range board1 {
+			for iy := range b {
+				if board1[ix][iy] != board2[ix][iy] {
+					return false
+				}
+			}
+		}
+	}
+	return result
 }
