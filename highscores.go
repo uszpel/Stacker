ca37143d73427f80313efdbaf2b4fc1ec417b5b4
Sven Uszpelkat <uszpelsv@cloudwws.com>
2025-10-23 12:02:19 +0200

    Added funcionality to edit HighScore

--- highscores.go
+++ highscores.go
@@ -2,22 +2,71 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
 type Score struct {
-	Rank  int    `json:"rank"`
 	Score int    `json:"score"`
 	Lines int    `json:"lines"`
 	Name  string `json:"name"`
+	Date  string `json:"date"`
 }
 
 type HighScore struct {
 	Scores []Score `json:"scores"`
 }
 
-func readHighscore(datafile string) (*HighScore, error) {
+func NewHighScore() *HighScore {
+	return &HighScore{
+		Scores: make([]Score, 0),
+	}
+}
+
+func NewScore(score int, lines int, name string) *Score {
+	return &Score{
+		Score: score,
+		Name:  name,
+		Lines: lines,
+		Date:  time.Now().UTC().String(),
+	}
+}
+
+func (h HighScore) CheckScore(score int) int {
+	result := -1
+	for index, curScore := range h.Scores {
+		if curScore.Score <= score {
+			result = index
+			break
+		}
+	}
+	return result
+}
+
+func (h *HighScore) InsertScore(score Score) {
+	index := h.CheckScore(score.Score)
+	if index == -1 {
+		h.Scores = append(h.Scores, score)
+	} else {
+		newScores := make([]Score, 0)
+		newScores = append(newScores, h.Scores[:index]...)
+		newScores = append(newScores, score)
+		newScores = append(newScores, h.Scores[index:]...)
+		h.Scores = newScores
+	}
+}
+
+func (h HighScore) PrintScores() string {
+	result := ""
+	for _, curScore := range h.Scores {
+		result += fmt.Sprintf("%d,", curScore.Score)
+	}
+	return result
+}
+
+func ReadHighscore(datafile string) (*HighScore, error) {
 	if _, err := os.Stat(datafile); err == nil {
 		result, err := os.ReadFile(datafile)
 		if err != nil {
@@ -30,17 +79,16 @@ func readHighscore(datafile string) (*HighScore, error) {
 		if err != nil {
 			return nil, err
 		}
+		//TODO: Check order
 		return data, nil
 	} else {
 		log.Printf("Highscore file %v not found", datafile)
 	}
 
-	return &HighScore{
-		Scores: make([]Score, 0),
-	}, nil
+	return NewHighScore(), nil
 }
 
-func writeHighscore(datafile string, highScore *HighScore) error {
+func WriteHighscore(datafile string, highScore *HighScore) error {
 	result, err := json.Marshal(highScore)
 	if err != nil {
 		return err
--- highscores_test.go
+++ highscores_test.go
@@ -9,7 +9,7 @@ const datafile = "highscores.test.data"
 
 func TestReadHighscoreWithoutFile(t *testing.T) {
 	os.Remove(datafile)
-	score, err := readHighscore(datafile)
+	score, err := ReadHighscore(datafile)
 	if err != nil {
 		t.Error(err)
 		t.FailNow()
@@ -20,10 +20,10 @@ func TestReadHighscoreWithoutFile(t *testing.T) {
 }
 
 func TestReadHighscoreWithFile(t *testing.T) {
-	writeHighscore(datafile, createTestScore())
+	WriteHighscore(datafile, createTestScore())
 	defer os.Remove(datafile)
 
-	score, err := readHighscore(datafile)
+	score, err := ReadHighscore(datafile)
 	if err != nil {
 		t.Error(err)
 		t.FailNow()
@@ -33,10 +33,46 @@ func TestReadHighscoreWithFile(t *testing.T) {
 	}
 }
 
-func createTestScore() *HighScore {
-	testscore := &HighScore{
-		Scores: make([]Score, 0),
+func TestCheckScoreWithFile(t *testing.T) {
+	WriteHighscore(datafile, createTestScore())
+	defer os.Remove(datafile)
+
+	score, err := ReadHighscore(datafile)
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
 	}
-	testscore.Scores = append(testscore.Scores, Score{Rank: 1, Score: 100, Name: "Test"})
+	if score.CheckScore(200) == -1 {
+		t.Errorf("Scores check for new record has failed")
+	}
+	if score.CheckScore(50) != -1 {
+		t.Errorf("Scores check has failed")
+	}
+}
+
+func TestInsertScoreWithFile(t *testing.T) {
+	WriteHighscore(datafile, createTestScore())
+	defer os.Remove(datafile)
+
+	score, err := ReadHighscore(datafile)
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	score.InsertScore(*NewScore(200, 0, "Test2"))
+	if score.PrintScores() != "200,100," {
+		t.Errorf("Insert new record failed")
+	}
+
+	score.InsertScore(*NewScore(50, 0, "Test3"))
+	if score.PrintScores() != "200,100,50," {
+		t.Errorf("Insert new score failed")
+	}
+}
+
+func createTestScore() *HighScore {
+	testscore := NewHighScore()
+	testscore.Scores = append(testscore.Scores, *NewScore(100, 0, "Test"))
 	return testscore
 }
